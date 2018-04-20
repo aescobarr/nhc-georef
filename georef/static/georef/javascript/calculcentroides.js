278a508c248a0802f1a04b9c9a3b796c1cf1621d
aescobarr <agustiescobar@gmail.com>
2018-04-20 15:04:03 +0200

    Search and centroid comp completed

--- georef/static/georef/javascript/calculcentroides.js
+++ georef/static/georef/javascript/calculcentroides.js
@@ -1,4 +1,43 @@
 $(document).ready(function() {
+
+    var show_data = function(centroid_x,centroid_y,unc){
+        $('#val_x').text(centroid_x);
+        $('#val_x').toggle( "highlight" );
+        $('#val_y').text(centroid_y);
+        $('#val_y').toggle( "highlight" );
+        $('#val_inc').text(unc);
+        $('#val_inc').toggle( "highlight" );
+    }
+
+    $( '#clipboard' ).click(function() {
+        var text = '';
+        text = 'lat:' + $('#val_y').text() + ' long:' + $('#val_x').text() + ' prec:' + $('#val_inc').text();
+        copyToClipboard(text);
+        toastr.success("Resultats copiats al portapapers!");
+    });
+
+    var computeCentroid = function(filename){
+        show_data('','','');
+        $.ajax({
+            url: _compute_centroid_url + encodeURI(filename),
+            method: "POST",
+            beforeSend: function(xhr, settings) {
+                if (!csrfSafeMethod(settings.type)) {
+                    var csrftoken = getCookie('csrftoken');
+                    xhr.setRequestHeader("X-CSRFToken", csrftoken);
+                }
+            },
+            success: function( data, textStatus, jqXHR ) {
+                 console.log(data);
+                 show_data(data.detail.centroid_x, data.detail.centroid_y, data.detail.inc);
+            },
+            error: function(jqXHR, textStatus, errorThrown){
+                toastr.error("Error calculant centroide - " + jqXHR.responseJSON.detail);
+                console.log(jqXHR);
+            }
+        });
+    }
+
     var uploader = new qq.FileUploader({
         action: _ajax_upload_url,
         element: $('#fileuploader')[0],
@@ -9,8 +48,9 @@ $(document).ready(function() {
         },
         onComplete: function(id, fileName, responseJSON) {
             if(responseJSON.success) {
-                $('#filename').val(responseJSON.filename);
-                toastr.success('Fitxer carregat al servidor amb èxit!')
+                //$('#filename').val(responseJSON.filename);
+                //toastr.success('Fitxer carregat al servidor amb èxit!')
+                computeCentroid(responseJSON.filename);
             } else {
                 toastr.error('Error pujant fitxer!')
             }
@@ -26,4 +66,5 @@ $(document).ready(function() {
             'csrf_xname': 'X-CSRFToken',
         }
     });
+
 });
